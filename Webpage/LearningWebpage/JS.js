8d0d40d0d3111515a8a1bd2516a156d34a28bd4b
aly468 <a82170010@gmail.com>
2025-08-17 14:17:59 +0800

    JS

---
+++ Webpage/LearningWebpage/JS.js
@@ -0,0 +1,101 @@
+
+
+
+
+
+
+/*
+//comparison
+alert(x == y);
+alert(x != y);
+alert(x === y);
+alert(x !== y);
+alert(x > y);
+alert(x < y);
+alert(x >= y);
+alert(x <= y);
+
+//Arrays
+// //let names =["Aly", "Liz", "Joy"];
+
+/*
+console.log(names[0]);
+names[names.length]="A.";
+names.push("DC ");
+names.unshift("1. ");
+*/
+
+//delete to desired length
+//names.length = 2;
+//delete at the end
+//names.pop();
+//delete at the beginning
+//names.shift();
+//delete like trim
+//names = names.slice(0,2);
+//alert(names);
+
+
+
+/*let $name = "Aly";
+console.log($name);
+
+//String Template Literals 
+//using backticks
+let sen = `Ana replied, "I don't care."`;
+let reply = `I said, "hi" and ${sen}`;
+console.log(reply);
+
+/*
+let  s = "I have a dog, my dog is cute.";
+//get the string using index (start,end)
+s = s.slice(2,6);
+alert(s)
+
+/*
+//one time relaced
+s = s.replace("dog", "cat");
+//all dog word is replaced
+c = s.replaceAll("dog", "cat");
+alert(s + "\n"+ c);
+
+let len = $name.length;
+alert(len);
+//index
+console.log($name[1]);
+
+/*
+let eng = 90;
+let math = 89;
+let sci = 75;
+let pe = 99;
+let psy = 99;
+
+alert("English: " + eng 
+    +"\nMathematics: " + math
+    +"\nScience: " + sci
+    +"\nPE: " + pe
+    +"\nPsychology: " + psy
+    +"\nAverage:" + (eng+math+sci+pe+psy)/5);
+
+
+let $nameC = "Alenere";
+let $neighbor = "David";
+let $pronounOfC = "she";
+let $drink = "coffee";
+
+//Concatenation
+alert($nameC +", the friendly neighbor, waved at " + $neighbor 
+    + " as " + $pronounOfC + " walked by " + $neighbor 
++ "'s house. " + $neighbor +" smiled back and invited "
++ $nameC + " in for a cup of " + $drink + ".")
+
+//using backticks
+alert(` ${$nameC} the friendly neighbor, waved at ${$neighbor} as ${$pronounOfC} walked by ${$neighbor}'s house. ${$neighbor} smiled back and invited ${$nameC} in for a cup of ${$drink}.`);
+
+
+//console.log("Hello World");
+//alert("Alert");
+document.getElementById("someText").innerHTML=$name;
+
+*/
